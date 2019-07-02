3068b8efbe99f6b70db9891f584e77ae52388d28
rzip84 <rzip84@naver.com>
2019-07-02 16:45:57 +0900

    함수형 컴포넌트 변경

--- src/pages/inc/LoginComplete.js
+++ src/pages/inc/LoginComplete.js
@@ -18,33 +18,3 @@ export function LoginComplete({loginInfo, handleLogOut}){
         </div>
     )
 }
-
-
-// class LoginComplete extends Component {
-//
-//
-//
-//     render() {
-//
-//         return (
-//             <div>
-//                 <Link className={styles['link']} to={`/${localStorage.getItem('user_type')}s/${localStorage.getItem('srl')}`}>영농일지</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-//                 <Link className={styles['link']} to="/mypage">마이페이지</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-//                 <a className={styles['link']} onClick={this.handleLogOut}>로그아웃</a>
-//             </div>
-//         )
-//     }
-//
-//     componentDidMount() {
-//        console.log("cmo...", localStorage.getItem('user_type'));
-//     }
-//
-// }
-// const mapDispatchToProps = (dispatch) => ({
-//     actions: bindActionCreators(importActions, dispatch),
-// })
-// const mapStateToProps = (state) => ({
-//     mapStateToProps: state.auth // state.variable_state 는 reducers/index.js 의 키값과 같아야 한다
-// })
-// export default connect(null, mapDispatchToProps)(LoginComplete);
-
