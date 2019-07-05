08b982f799c64db7e16ba20d7de6fab4acd141e2
rzip84 <rzip84@naver.com>
2019-07-05 13:47:55 +0900

    개인정보처리방침 첨부

--- src/pages/inc/Footer.js
+++ src/pages/inc/Footer.js
@@ -9,12 +9,12 @@ const Footer = () => {
         <footer className={styles['footer']}>
             <p>
                 Email : <a href="mailto:yspark@meteopark.dev">yspark@meteopark.dev</a><br/>
-                © 2019 <b>METEOPARK</b> All rights reserved.
-
-                <br/><br/>
-                OpenApi 사용처<br/><br/>
-                <img src="/images/banner/mafra_banner.jpg" className={styles['footer_banner']} alt="배너"/>&nbsp;&nbsp;
-                <img src="/images/banner/nongsaro_banner.jpg" className={styles['footer_banner']} alt="배너"/>
+                © 2019 <b>METEOPARK</b> All rights reserved.<br/><br/>
+                <a href="/privacy.html" className={styles['link-w']} target="_blank">개인정보처리방침</a>
+                {/*<br/><br/>*/}
+                {/*OpenApi 사용처<br/><br/>*/}
+                {/*<img src="/images/banner/mafra_banner.jpg" className={styles['footer_banner']} alt="배너"/>&nbsp;&nbsp;*/}
+                {/*<img src="/images/banner/nongsaro_banner.jpg" className={styles['footer_banner']} alt="배너"/>*/}
             </p>
             {/*<p>*/}
             {/*    <reactIconFa.FaLaravel color="#F15F5F" size="1.5em" className={styles['icon']}/>*/}
