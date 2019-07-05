596af2188e0fbeb2a8823f1ab5733af22c6c4820
rzip84 <rzip84@naver.com>
2019-07-05 14:32:46 +0900

    bug fix

--- src/pages/mentees/Mentee.js
+++ src/pages/mentees/Mentee.js
@@ -54,21 +54,24 @@ class Mentee extends Component {
         const {actionMentee, match} = this.props;
         actionMentee.getMentee(match.params.mentee);
         actionMentee.getMenteeDiaries(match.params.mentee, 1);
+
+
+
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        /*
-        이 API는 컴포넌트에서 render() 를 호출하고난 다음에 발생하게 됩니다. 이 시점에선 this.props 와 this.state 가 바뀌어있습니다.
-        그리고 파라미터를 통해 이전의 값인 prevProps 와 prevState 를 조회 할 수 있습니다.
-        그리고, getSnapshotBeforeUpdate 에서 반환한 snapshot 값은 세번째 값으로 받아옵니다.
-         */
-        if (this.props.match.params.mentee !== prevProps.match.params.mentee) {
-
-            const {actionMentee, match} = this.props;
-            actionMentee.getMentee(match.params.mentee);
-            actionMentee.getMenteeDiaries(match.params.mentee, 1);
-        }
-    }
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //     /*
+    //     이 API는 컴포넌트에서 render() 를 호출하고난 다음에 발생하게 됩니다. 이 시점에선 this.props 와 this.state 가 바뀌어있습니다.
+    //     그리고 파라미터를 통해 이전의 값인 prevProps 와 prevState 를 조회 할 수 있습니다.
+    //     그리고, getSnapshotBeforeUpdate 에서 반환한 snapshot 값은 세번째 값으로 받아옵니다.
+    //      */
+    //     if (this.props.match.params.mentee !== prevProps.match.params.mentee) {
+    //
+    //         const {actionMentee, match} = this.props;
+    //         actionMentee.getMentee(match.params.mentee);
+    //         actionMentee.getMenteeDiaries(match.params.mentee, 1);
+    //     }
+    // }
 }
 
 const mapStateToProps = (state) => ({
--- src/pages/mentors/Mentor.js
+++ src/pages/mentors/Mentor.js
@@ -52,19 +52,19 @@ class Mentor extends Component {
         actionMentor.getMentorDiaries(match.params.mentor, 1);
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        /*
-        이 API는 컴포넌트에서 render() 를 호출하고난 다음에 발생하게 됩니다. 이 시점에선 this.props 와 this.state 가 바뀌어있습니다.
-        그리고 파라미터를 통해 이전의 값인 prevProps 와 prevState 를 조회 할 수 있습니다.
-        그리고, getSnapshotBeforeUpdate 에서 반환한 snapshot 값은 세번째 값으로 받아옵니다.
-         */
-        if (this.props.match.params.mentor !== prevProps.match.params.mentor) {
-
-            const {actionMentor, match} = this.props;
-            actionMentor.getMentor(match.params.mentor);
-            actionMentor.getMentorDiaries(match.params.mentor, 1);
-        }
-    }
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //     /*
+    //     이 API는 컴포넌트에서 render() 를 호출하고난 다음에 발생하게 됩니다. 이 시점에선 this.props 와 this.state 가 바뀌어있습니다.
+    //     그리고 파라미터를 통해 이전의 값인 prevProps 와 prevState 를 조회 할 수 있습니다.
+    //     그리고, getSnapshotBeforeUpdate 에서 반환한 snapshot 값은 세번째 값으로 받아옵니다.
+    //      */
+    //     if (this.props.match.params.mentor !== prevProps.match.params.mentor) {
+    //
+    //         const {actionMentor, match} = this.props;
+    //         actionMentor.getMentor(match.params.mentor);
+    //         actionMentor.getMentorDiaries(match.params.mentor, 1);
+    //     }
+    // }
 }
 
 const mapStateToProps = (state) => ({
