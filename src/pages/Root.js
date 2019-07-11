0e2f9886fb09a7b2a2d9e2b11ba3c21745327c26
rzip84 <rzip84@naver.com>
2019-07-11 15:03:05 +0900

    ie 대응

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -49,7 +49,7 @@ class Root extends React.Component {
                     <Header/>
                     <div className={styles['wrapper']}>
                         {/*<SideNav />*/}
-                        <div className={styles['main']}>
+                        <main role="main">
                             <Switch>
 
                                 {this.props.mapStateToPropsAuth.is_logged && <Route exact path="/mentees/:mentee/create" component={MenteeDiaryCreate}/>}
@@ -88,7 +88,7 @@ class Root extends React.Component {
                             <div className={styles['float-button-position']}>
                                 <div className={styles['float-button']} onClick={() => {this.scrollToTop();}}><reactIconFa.FaChevronUp/></div>
                             </div>
-                        </div>
+                        </main>
                         <Footer/>
                     </div>
                 </div>
