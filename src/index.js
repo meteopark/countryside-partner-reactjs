b771cda1725353875b1892b5ff077506f7b09e33
rzip84 <rzip84@naver.com>
2019-07-11 18:54:31 +0900

    bug 수정 중

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import "babel-polyfill";
+import "isomorphic-fetch";
+
+
+
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/ie9';
+// import 'react-app-polyfill/stable';
+
+
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
 import {Provider} from "react-redux";
 import {store} from "./store";
 import Root from "./pages/Root";
